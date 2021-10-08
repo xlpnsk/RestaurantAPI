8dde3667e22e1a0907186707436587741c2b4c1c
xlpnsk <adam.lipinski@pollub.edu.pl>
2021-10-08 22:06:02 +0200

    Lecture 23 - Create Error Handling Middleware

--- RestaurantAPI2/Startup.cs
+++ RestaurantAPI2/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestaurantAPI2.Entities;
+using RestaurantAPI2.Middleware;
 using RestaurantAPI2.Services;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,7 @@ namespace RestaurantAPI2
             services.AddScoped<RestaurantSeeder>();
             services.AddAutoMapper(this.GetType().Assembly);
             services.AddScoped<IRestaurantService, RestaurantService>();
+            services.AddScoped<ErrorHandlingMiddleware>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +45,7 @@ namespace RestaurantAPI2
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseHttpsRedirection();
 
